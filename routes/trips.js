bde6793e7f0c5b083035f72ea20a246ed1f76371
Your name <Your email>
2025-04-15 12:55:03 +0200

    route POST created

--- routes/trips.js
+++ routes/trips.js
@@ -1,18 +1,26 @@
-const express = require("express");
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
 
 const Trip = require("../models/trips");
+const moment = require("moment")
 
 router
-  .get("/", (req, res) => {
+  .post("/", (req, res) => {
+    //pattern = /[0-9]*-[0-9]*-[0-9]*/
+    let formatDate = new Date(req.body.date)
+    
+  //  console.log(formatDate)
+ 
+    formatDate = moment(formatDate).format('YYYY-MM-DD')
+    
+    
     Trip.find({
       departure: req.body.departure,
       arrival: req.body.arrival,
-      date: req.body.arrival,
-    });
-  })
+      date: {$gte:moment(formatDate).startOf('day'), $lte: moment(formatDate).endOf('day') }
+    })
   .then(data => {
     res.json({trips :data, result: true})
   })
-
+ })
 module.exports = router;

e3feb3673cdd2bc982e419fc3533c9561d4bf237
paulinepasseri <paulinecpasseri@gmail.com>
2025-04-15 14:28:37 +0200

    condition false added in router.post /trips

--- routes/trips.js
+++ routes/trips.js
@@ -6,21 +6,20 @@ const moment = require("moment")
 
 router
   .post("/", (req, res) => {
-    //pattern = /[0-9]*-[0-9]*-[0-9]*/
     let formatDate = new Date(req.body.date)
-    
-  //  console.log(formatDate)
- 
     formatDate = moment(formatDate).format('YYYY-MM-DD')
-    
-    
-    Trip.find({
-      departure: req.body.departure,
-      arrival: req.body.arrival,
-      date: {$gte:moment(formatDate).startOf('day'), $lte: moment(formatDate).endOf('day') }
+    if ((!req.body.departure) || (!req.body.arrival)) {
+      res.json({result: false})
+    } else {
+      Trip.find({
+        departure: req.body.departure,
+        arrival: req.body.arrival,
+        date: {$gte:moment(formatDate).startOf('day'), $lte: moment(formatDate).endOf('day') }
+      })
+    .then(data => {
+      res.json({trips :data, result: true})
     })
-  .then(data => {
-    res.json({trips :data, result: true})
-  })
+    }  
  })
+
 module.exports = router;
